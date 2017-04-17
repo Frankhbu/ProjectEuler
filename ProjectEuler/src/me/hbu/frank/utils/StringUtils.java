8de8b014df8cd4b8dae0b9d68758fb0588302492
Frankhbu <522547470@qq.com>
2017-04-17 14:50:49 +0800

    添加大数相乘算法

--- ProjectEuler/src/me/hbu/frank/utils/StringUtils.java
+++ ProjectEuler/src/me/hbu/frank/utils/StringUtils.java
@@ -2,4 +2,89 @@ package me.hbu.frank.utils;
 
 public class StringUtils {
 
+	/**
+	 * 计算两个数a和b的乘积
+	 * @param a
+	 * @param b
+	 * @return c
+	 */
+	public static String mutiply(String a,String b){
+		
+		int lengthA = a.length();
+		int lengthB = b.length();
+		
+		char[] charArrayA = a.toCharArray();
+		char[] charArrayB = b.toCharArray();
+		
+		int[] intArrayA = getIntArray(charArrayA);
+		int[] intArrayB = getIntArray(charArrayB);
+		intArrayA = transposition(intArrayA);
+		intArrayB = transposition(intArrayB);
+		
+		int lengthC = lengthA + lengthB - 1;
+		int[] intArrayC = new int[lengthC];
+		
+		for (int i = 0; i < intArrayB.length; i++) {
+			for (int j = 0; j < intArrayA.length; j++) {
+				int temp = intArrayB[i] * intArrayA[j];
+				temp += intArrayC[i+j];
+				if (temp > 9) {
+					intArrayC[i+j] = temp % 10;
+					intArrayC[i+j+1] += temp / 10; 
+				}else {
+					intArrayC[i+j] = temp;
+				}
+			}
+		}
+		
+		return getTranspositionedString(intArrayC);
+	}
+
+	/**
+	 * 将数组转置，并返回转置后的字符串（去前导0）
+	 * @param array
+	 * @return
+	 */
+	private static String getTranspositionedString(int[] array) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = array.length - 1; i >= 0; i--) {			
+			sb.append(array[i]);
+		}
+		return sb.toString().replaceFirst("^0+", "");
+	}
+
+
+	/**
+	 * 对数组进行转置
+	 * @param intArrayA
+	 * @return
+	 */
+	private static int[] transposition(int[] intArrayA) {
+		int len = intArrayA.length;
+		int[] intArray = new int[len];
+		for (int i = 0; i < intArrayA.length; i++) {
+			intArray[i] = intArrayA[len - 1 - i];
+		}
+		return intArray;
+	}
+
+	/**
+	 * 将字符数组转换为整形数组
+	 * @param charArrayA
+	 * @return
+	 */
+	private static int[] getIntArray(char[] charArrayA) {
+		int len = charArrayA.length;
+		int[] intArray = new int[len];
+		for (int i = 0; i < charArrayA.length; i++) {
+			intArray[i] = charArrayA[i] - 48;
+		}
+		return intArray;
+	}
+
+	public static void main(String[] args) {
+		String a = "1111111111";
+		String b = "1111111111";
+		System.out.println(mutiply(a, b));
+	}
 }
